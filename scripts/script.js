bb8bca2f1275db8b442a280abf500dc7db28fc32
Jaina Barreto <46823866+jaibrr@users.noreply.github.com>
2022-02-12 20:27:33 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+/* 
+Quanto tempo dura o churrasco
+men cis:
+Carnes: 200g
+Frango: 200g
+Porco: 200g
+Cerveja: 2 litro
+Refrigerante: 700 ml
+Água: 1 litro
+
+women cis: 
+Carnes: 150g
+Frango: 150g
+Porco: 100g
+Cerveja: 1,5 litro 
+Refrigerante: 500 ml
+Água: 1 litro
+
+vegs
+Tomate: 50g
+Abobrinha: 50g
+Sal 18g para 1kg de carne
+
+Acompanhamentos:
+Pão de alho
+Pão francês: 100g
+Maionese: 100g
+Alho: 2g
+Vinagrete
+Pimentão: 20g
+Cebola: 20g
+queijo coalho: 50g
+Arroz: 80g
+
+
+Crianças:
+mulher/2
+
+Duração do evento
+ */
+
+let inputWomen = document.getElementById('women');
+let inputMen = document.getElementById('men');
+let inputKids = document.getElementById('kids');
+
+function check(){
+    if(!isNaN(num)) alert("a variavel num é numérica");
+}
+
+function calc(){
+    console.log("Calculating...")
+
+    let wonMeat = 0.4
+    let menMeat = 0.6
+    let wonBeer = 1
+    let menBeer = 1.5
+    let side = .25
+    let wonNoAlc = 0.5
+    let menNoAlc = 0.8
+
+    let women = inputWomen.value
+    let men = inputMen.value
+    let kids = inputKids.value
+
+    let qtyMeat = women * wonMeat + men * menMeat + kids * wonMeat/2
+    let qtyBeer = women * wonBeer + men * menBeer
+    let qtyNoAlc = women * wonNoAlc + men * menNoAlc + kids * wonNoAlc/2
+    let qtySide = women * side + men * side + kids * side
+
+    result.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`
+    result.innerHTML += `<div class="result-block"><p> ${qtyMeat} kg de carne</p></div>`
+    result.innerHTML += `<div class="result-block"><p> ${qtyBeer} litros de cerveja</p></div>`
+    result.innerHTML += `<div class="result-block"><p>${qtyNoAlc} litros de bebidas não alcoolica</p></div>`
+    result.innerHTML += `<div class="result-block"><p>${qtySide} kg de acompanhamentos</p></div>`
+    
+}
